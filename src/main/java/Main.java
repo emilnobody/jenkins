b9a133f0126df51054922bf5185bd9c847840427
Kasper <s57522@beuth-hochschule.de>
2016-04-26 19:56:17 +0200

    added somthing to main (2)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,5 +10,6 @@ public class Main {
         System.out.println(Utils.multiplyBy4(4));
         System.out.println("Test: 3 is even =");
         System.out.println(Utils.isEven(3));
+        System.out.println("Demo Ende");
     }
 }
