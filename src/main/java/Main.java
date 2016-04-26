e12113808c459e33bd37534ad66ca6499b42256f
Kasper <s57522@beuth-hochschule.de>
2016-04-26 19:53:38 +0200

    added somthing to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,9 @@ package main.java;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, ich bin das Jenkins Demo Projekt.");
+        System.out.println("Test: 4x4=");
         System.out.println(Utils.multiplyBy4(4));
+        System.out.println("Test: 3 is even =");
         System.out.println(Utils.isEven(3));
     }
 }
